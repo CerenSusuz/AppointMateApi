13344006267c43b00b4c7c477572b8f18701557b
ceren <ceren199704@hotmail.com>
2023-12-06 00:36:35 +0300

    created enums

---
+++ src/Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointMateApi.Domain.Enums;
+public enum AppointmentStatus
+{
+    Pending = 0,
+    Confirmed = 1,
+    Cancelled = 2
+}
